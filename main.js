fd72e6823df737325cba97cc590d7a958a9635dd
Ale <alerdo23@gmail.com>
2023-06-19 16:39:42 +0100

    Add Icons and style them

--- main.js
+++ main.js
@@ -4,29 +4,38 @@
 //Using javascript to change the backgrond of nav links while scrolling down or cliking
 
 window.addEventListener('scroll', () => {
-      const nav = document.querySelector('nav');
-      const sections = document.querySelectorAll('section');
-      let currentSection = '';
+  const nav = document.querySelector('nav');
+  const sections = document.querySelectorAll('section');
+  let currentSection = '';
 
-      sections.forEach( section => {
-        var sectionTop = section.offsetTop; //It represents the distance between the top of an element and the top of its offset parent
-        var sectionHeight = section.clientHeight; //Height of section
-        let diffrence = sectionTop - (sectionHeight/4)
-        if (window.pageYOffset >= sectionTop - sectionHeight / 4) {
-          currentSection = section.getAttribute('id');  //Get id name of currrent section which is the same href for the links
-          
-}}); 
- 
-        var links = document.querySelectorAll('nav a');
-        links.forEach(link => {
-        link.style.color = '';
-        link.style.backgroundColor = ''; // Reset values we want to change 
-        if (link.getAttribute('href').substring(1) === currentSection) {
-            link.style.color = 'white'; 
-            link.style.backgroundColor = '#2EE59D'; //Set new values for the changed ones
-        }
-        });
-    });
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    let difference = sectionTop - (sectionHeight / 4);
+    if (window.pageYOffset >= difference) {
+      currentSection = section.getAttribute('id');
+    }
+  });
+
+  let icons = document.querySelectorAll("i");
+  var links = document.querySelectorAll('nav a');
+
+  links.forEach((link, index) => {
+    link.style.color = '';
+    link.style.backgroundColor = '';
+
+    if (link.getAttribute('href').substring(1) === currentSection) {
+      link.style.color = 'white';
+      link.style.backgroundColor = '#2EE59D';
+
+      icons[index].style.color = 'white';
+    } else {
+    
+      
+      icons[index].style.color = '';
+    }
+  });
+});
 
 
 
